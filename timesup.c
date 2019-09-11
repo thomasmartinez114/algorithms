5157fc2a11aad90d8ff954ac9ed03a31c680be28
tmartinez114 <tmartinez114@gmail.com>
2019-09-10 20:31:20 -0400

    incrementing days in if statement

--- timesup.c
+++ timesup.c
@@ -14,21 +14,26 @@
 
 int main(void)
 {
-    // Prompt user for x starting hour between 1 and 12
-    int x = 3; //get_int("Starting Hour is between 1 - 12");
+    // x starting hour between 1 and 12
+    int x = 3;
 
-    // Prompt user for y hours passed between 1 and int_max
-    int y = 5; // get_int("Time passed in hours is: ");
+    // y hours passed between 1 and int_max
+    int y = 5;
 
     int time = x + y; // 8
 
     int days;
 
+    // Does time equal 24 hours -- check if time = 24 = 1 day
+    // If it is full day increment the days || days++
+    // If doesn't modulo the remaining time
+
+    if (time >= 24)
+    {
+        days++;
+    }
+
     // Check constraints of input
-    //     if (0 <= x <= 12)
-    //     {
-    //         printf("Starting time is %i\n", x);
-    //     }
 
     // Convert the hours input to minutes to then add together and then convert back to hours
     //
