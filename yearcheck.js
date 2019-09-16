c0e84fd679483d4c50963acd4abf4e7b575970dd
tmartinez114 <tmartinez114@gmail.com>
2019-09-16 18:13:08 -0400

    console logged the current month and year

--- yearcheck.js
+++ yearcheck.js
@@ -1,3 +1,9 @@
 // current month / year - old month / year
 // give number of years past
 // 09/19 - 03/15 = 4.5 years
+// current time should be updated automatically not hard coded
+// static it first then make it dynamic
+var currentYear = new Date().getFullYear();
+var currentMonth = new Date().getMonth() + 1;
+
+console.log(`${currentMonth} & ${currentYear}`);
