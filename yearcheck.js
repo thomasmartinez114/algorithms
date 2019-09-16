8e75c738e7ae394775bff9306a21c53eafbc9d72
tmartinez114 <tmartinez114@gmail.com>
2019-09-16 18:30:53 -0400

    implemented readline to get userinput

--- yearcheck.js
+++ yearcheck.js
@@ -3,7 +3,14 @@
 // 09/19 - 03/15 = 4.5 years
 // current time should be updated automatically not hard coded
 // static it first then make it dynamic
+
+var readline = require('readline-sync');
+
+var enterMonth = readline.question('Enter a month in 00 format: ');
+console.log(enterMonth);
+
 var currentYear = new Date().getFullYear();
 var currentMonth = new Date().getMonth() + 1;
 
 console.log(`${currentMonth} & ${currentYear}`);
+console.log(enterMonth);
