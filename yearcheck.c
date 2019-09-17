2a03ca9bc4e45741077e4298c21fbafdb3e98749
tmartinez114 <tmartinez114@gmail.com>
2019-09-16 23:27:00 -0400

    set int for static month/year

--- yearcheck.c
+++ yearcheck.c
@@ -1,6 +1,15 @@
+// https : //stackoverflow.com/questions/1442116/how-to-get-the-date-and-time-values-in-a-c-program
+
 #include <stdio.h>
 #include <cs50.h>
 
 int main(void)
 {
+    int pastMonth = 6;
+
+    int currMonth = 9;
+
+    int pastYear = 1992;
+
+    int currYear = 2019;
 }
