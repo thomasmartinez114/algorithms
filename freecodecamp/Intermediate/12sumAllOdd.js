81554442ce3365ca94e5913b87556c3223e5b91e
tmartinez114 <tmartinez114@gmail.com>
2019-09-18 09:20:05 -0400

    pushed Intermediates --- not completed

---
+++ freecodecamp/Intermediate/12sumAllOdd.js
@@ -0,0 +1,13 @@
+// Intermediate Algorithm Scripting: Sum All Odd Fibonacci Numbers
+
+// Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+
+// The first two numbers in the Fibonacci sequence are 1 and 1. Every additional number in the sequence is the sum of the two previous numbers.The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+
+// For example, sumFibs(10)should return 10because all odd Fibonacci numbers less than or equal to 10are 1, 1, 3, and 5.
+
+// Remember to use Read - Search - Ask if you get stuck.Try to pair program.Write your own code.
+
+do {
+  stuff;
+} while (something);
---
+++ freecodecamp/Intermediate/13sumAllPrimes.js
@@ -0,0 +1,22 @@
+// Intermediate Algorithm Scripting: Sum All Primes
+
+// Sum all the prime numbers up to and including the provided number.
+
+// A prime number is defined as a number greater than one and having only two divisors, one and itself.For example, 2 is a prime number because it's only divisible by one and two.
+
+// The provided number may not be a prime.
+
+// Remember to use Read - Search - Ask if you get stuck.Try to pair program.Write your own code.
+
+function sumPrimes(num) {
+  if (num > 1) {
+    var sum = 0;
+
+    for (var i = 0; i < num.length; i++) {
+      // if the number isn't divisible by 1 or two
+      // break
+    }
+  }
+}
+
+sumPrimes(10);
