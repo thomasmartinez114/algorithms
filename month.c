e197733fedb1804b0b2565fc06fd75a3f0b744b6
tmartinez114 <tmartinez114@gmail.com>
2019-09-19 11:14:18 -0400

    updated comment with link -- added struct

--- month.c
+++ month.c
@@ -1,6 +1,12 @@
+// Following tutorial below on how to implement month and year
+// https : //www.youtube.com/watch?v=4j0pRe8rxhs
+
 #include <stdio.h>
 #include <cs50.h>
 #include <time.h>
+
+// Define struct
+typedef struct tm tm;
 
 int main(void)
 {
@@ -8,4 +14,7 @@ int main(void)
     time(&currentTime);
 
     printf("%s\n", ctime(&currentTime));
+
+    // Access what month it is
+    tm *myTime = localtime(&currentTime);
 }
