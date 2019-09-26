bc6e39e4404964cacf62cb8db99e7e168e772574
tmartinez114 <tmartinez114@gmail.com>
2019-09-26 12:12:10 -0400

    made an if function

--- freecodecamp/Basic/06confirmEnding.js
+++ freecodecamp/Basic/06confirmEnding.js
@@ -1,9 +1,20 @@
 function confirmEnding(str, target) {
-  console.log(str.substr(-target.length)); // logs 'n'
+  // console.log(str.substr(-1)); // logs substr(-1) = 'n'
+  if (str.substr(-target.length) === target) {
+    console.log('True');
+  } else {
+    console.log('False');
+  }
 }
 
-confirmEnding('Bastian', 'n');
+confirmEnding('Thomas', 'mas');
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substr
 
 // The substr() method returns a portion of the string, starting at the specified index and extending for a given number of characters afterward.
+
+// str.substr(start[, length])
+
+// ParametersSection
+//   start - The index of the first character to include in the returned substring.
+//   length - Optional.The number of characters to extract.
