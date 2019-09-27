e945a0f4afe2eaf899676d4b331d0ce338c71fd0
tmartinez114 <tmartinez114@gmail.com>
2019-09-26 21:26:49 -0400

    need to figure out the .length portion of the challenge

---
+++ freecodecamp/Basic/08truncateString.js
@@ -0,0 +1,18 @@
+// Basic Algorithm Scripting: Truncate a String
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+
+// Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+function truncateString(str, num) {
+  var eachLetter = str.slice(0, num);
+  // console.log(eachLetter); // log out the letters by what num - 1 is
+  var newStr = eachLetter + '...';
+  console.log(newStr); // log what the words would be with '...' added
+
+  console.log(num);
+}
+
+truncateString(
+  'A-tisket a-tasket A green and yellow basket',
+  'A-tisket a-tasket A green and yellow basket'.length
+);
