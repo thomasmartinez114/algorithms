6d54eb8f7af4e8c270951b4731ca0d8982a17890
tmartinez114 <tmartinez114@gmail.com>
2019-09-30 12:12:52 -0400

    still cant get mine to work but the hints version below does

--- freecodecamp/Basic/11titleCaseASentence.js
+++ freecodecamp/Basic/11titleCaseASentence.js
@@ -19,3 +19,26 @@ titleCase("I'm a little tea pot");
 // https://flaviocopes.com/how-to-uppercase-first-letter-javascript/
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
 // https://flaviocopes.com/how-to-uppercase-first-letter-javascript/
+
+\\\\\\\\\\\\\\\\\
+\\ This works  \\
+\\\\\\\\\\\\\\\\\
+
+
+// function titleCase(str) {
+//   str = str.toLowerCase();
+//   console.log(str);
+
+//   var newWords = str.split(' ');
+//   console.log(newWords);
+
+//   for (var i = 0; i < newWords.length; i++) {
+//     newWords[i] = newWords[i].charAt(0).toUpperCase() + newWords[i].slice(1);
+//     console.log(newWords[i])
+//   }
+//   str = newWords.join(' ');
+//   console.log(str);
+//   return str;
+// }
+
+// titleCase("I'm a little tea pot");
