c74f53e3983171a495adb9542cecba131b43867a
tmartinez114 <tmartinez114@gmail.com>
2019-09-29 23:05:12 -0400

    used chatAt() to cap first letter -- need to join the words

--- freecodecamp/Basic/11titleCaseASentence.js
+++ freecodecamp/Basic/11titleCaseASentence.js
@@ -1,11 +1,15 @@
 function titleCase(str) {
-  var newStr = str.split(' ');
-  console.log(newStr);
-  for (var i = 0; i < newStr.length; i++) {
-    //console.log(newStr[i]);
-    var upper = newStr[i][0].toUpperCase();
-    console.log(upper);
+  var words = str.split(' ');
+  console.log(words);
+
+  for (var i = 0; i < words.length; i++) {
+    var capWords = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    console.log(capWords);
   }
 }
 
 titleCase("I'm a little tea pot");
+
+// https://flaviocopes.com/how-to-uppercase-first-letter-javascript/
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+// https://flaviocopes.com/how-to-uppercase-first-letter-javascript/
