50d9d4ffeb30f2e39f21d6ab247780d6414ff18d
tmartinez114 <tmartinez114@gmail.com>
2019-09-30 16:06:39 -0400

    completed 12

--- freecodecamp/Basic/12sliceAndSplice.js
+++ freecodecamp/Basic/12sliceAndSplice.js
@@ -2,12 +2,15 @@ function frankenSplice(arr1, arr2, n) {
   // It's alive. It's alive!
   var copyArr1 = arr1.slice(0);
   var copyArr2 = arr2.slice(0);
-  console.log(copyArr2);
+  console.log("Arr1: " + copyArr1);
+  console.log("Arr2: " + copyArr2);
 
   for (var i = 0; i < copyArr1.length; i++) {
-    copyArr2.push(copyArr1[i]);
+    copyArr2.splice(n, 0, copyArr1[i]);
+    n++; // increment index position to place the element in proper index
   }
   console.log(copyArr2);
+  return copyArr2;
 }
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1);
