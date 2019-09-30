c018fb299ecd143a761cde4d130c7dc27ef3f0d0
tmartinez114 <tmartinez114@gmail.com>
2019-09-29 23:17:19 -0400

    got the words with cap now need to join them

--- freecodecamp/Basic/11titleCaseASentence.js
+++ freecodecamp/Basic/11titleCaseASentence.js
@@ -1,11 +1,15 @@
 function titleCase(str) {
   var words = str.split(' ');
   console.log(words);
+  var newWords = [];
 
   for (var i = 0; i < words.length; i++) {
     var capWords = words[i].charAt(0).toUpperCase() + words[i].slice(1);
     console.log(capWords);
+    newWords.push(capWords);
+    console.log(newWords);
   }
+  return newWords;
 }
 
 titleCase("I'm a little tea pot");
