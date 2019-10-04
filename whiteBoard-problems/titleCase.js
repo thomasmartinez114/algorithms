c6e4e9339e52c66a0a0e4948c897fae0bd1fb40d
tmartinez114 <tmartinez114@gmail.com>
2019-10-03 21:06:27 -0400

    corrected code

--- whiteBoard-problems/titleCase.js
+++ whiteBoard-problems/titleCase.js
@@ -6,13 +6,37 @@
 // lowercase all into an arrary
 // take the first letter of each and cap
 
-function titleCase(sentence) {
-    let newSent = sentence.toLowerCase();
-    let words = newSent.split(' '); // splits by space aka each word and places into an array
+//////////////////
+//  Corrected   //
+//////////////////
 
-    for (let letters = 0; letters < words.length; letters++) {
-        wordsCap = words[letters].charAt(0).toUpperCase() + words[letters].slice(1); // cap first letter with charAt(0) then add with rest of letters after char 0
+function titleCase(str) {
+    let words = str.toLowerCase().split(' ');
+    console.log(words);
+
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+        console.log(words);
     }
-    console.log(wordsCap);
-    return wordsCap;
+    console.log(words.join(' '));
+    return words.join(' ');
 }
+
+titleCase("I'm a little tea pot");
+
+//////////////////////
+//  First Attempt   //
+//////////////////////
+
+// function titleCase(sentence) {
+//     let newSent = sentence.toLowerCase();
+//     let words = newSent.split(' '); // splits by space aka each word and places into an array
+
+//     for (let letters = 0; letters < words.length; letters++) {
+//         wordsCap = words[letters].charAt(0).toUpperCase() + words[letters].slice(1); // cap first letter with charAt(0) then add with rest of letters after char 0
+//     }
+//     console.log(wordsCap);
+//     return wordsCap;
+// }
+
+// titleCase("I'm a little tea pot");
