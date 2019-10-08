63865af16d89c9e64d2339136f8f15885aca4842
tmartinez114 <tmartinez114@gmail.com>
2019-10-08 19:43:18 -0400

    first attempt

--- whiteBoard-problems/countingValleys.js
+++ whiteBoard-problems/countingValleys.js
@@ -2,7 +2,22 @@
 // if steps == D then subtract 1
 // if steps == U then add 1
 
+function countingValleys(n, s) {
+    let hike = 0;
+    // CONST D = -1;
+    // CONST U = 1;
 
+    for (let steps = 0; steps < s.length; steps++) {
+        if (s[steps] === 'D') {
+            hike--;
+        } else if (s[steps] === 'U') {
+            hike++;
+        } else {
+            break;
+        }
+    }
+    console.log(hike);
+}
 
 
 
