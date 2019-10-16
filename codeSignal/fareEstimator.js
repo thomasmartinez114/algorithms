9ec11b911de093129702f2aff46ef0872c16bb95
tmartinez114 <tmartinez114@gmail.com>
2019-10-16 01:07:14 -0400

    began fareEstimator

---
+++ codeSignal/fareEstimator.js
@@ -0,0 +1,10 @@
+// https://app.codesignal.com/company-challenges/uber/HNQwGHfKAoYsz9KX6
+
+function fareEstimator(
+  ride_time,
+  ride_distance,
+  cost_per_minute,
+  cost_per_mile
+) {}
+
+fareEstimator(30, 7, [0.2, 0.35, 0.4, 0.45], [1.1, 1.8, 2.3, 3.5]);
