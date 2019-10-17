6a0f5fbd5d9002b4be5b59d6834eb3ff5da65993
tmartinez114 <tmartinez114@gmail.com>
2019-10-16 23:58:55 -0400

    for loops

--- codeSignal/fareEstimator.js
+++ codeSignal/fareEstimator.js
@@ -1,19 +1,34 @@
 // https://app.codesignal.com/company-challenges/uber/HNQwGHfKAoYsz9KX6
 
-function fareEstimator() {
+function fareEstimator(
+  ride_time,
+  ride_distance,
+  cost_per_minute,
+  cost_per_mile
+) {
   // input int for ride_time
   // input int for ride_distance
 
   // ((cost per min * ride time) + (cost per mile * ride dist))
+  // cost per min & cost per mile depend on car type
 
-  let ridePrice = [];
-  let ride_time = 30;
-  let ride_distance = 7;
-  let cost_per_minute = 10;
-  let cost_per_mile = 1;
+  //     let ridePrice = [];
+  //     let ride_time = 30;
+  //     let ride_distance = 7;
+  //     let cost_per_minute = 10;
+  //     let cost_per_mile = 1;
 
-  let fare = cost_per_minute * ride_time + cost_per_mile * ride_distance;
-  console.log(`Total cost for ride is: $${fare}`);
+  //     let fare = ((cost_per_minute * ride_time) + (cost_per_mile * ride_distance));
+  //     console.log(fare);
+
+  for (let car = 0; car < cost_per_minute.length; car++) {
+    console.log(cost_per_minute[car]);
+  }
+
+  for (let car = 0; car < cost_per_mile.length; car++) {
+    let mileage = cost_per_mile[car];
+    console.log(mileage);
+  }
 }
 
-fareEstimator();
+fareEstimator(10, 5, 1.5, 1.0);
