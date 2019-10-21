3a7f16940c91e0448c00020b11c28ea8ce79f626
tmartinez114 <tmartinez114@gmail.com>
2019-10-21 18:33:49 -0400

    completed 01 in 09:03

--- freecodecamp-t/Intermediate/01sumNumbersRange.js
+++ freecodecamp-t/Intermediate/01sumNumbersRange.js
@@ -5,23 +5,49 @@
 // Remember to use Read - Search - Ask if you get stuck.Try to pair program.Write your own code.
 
 function sumAll(arr) {
-  // sort the array smallest - largest
-  arr.sort(function(a, b) {
-    return a - b;
-  });
-  console.log(arr);
+  // Sort the arr of numbers from least to greatest
+  // Then loop through to add all numbers between them
 
   let sum = 0;
+  let sortedArr = arr.sort(function(a, b) {
+    return a - b;
+  });
+  console.log(sortedArr);
 
-  for (let num = arr[0]; num <= arr[1]; num++) {
-    // console.log(num);
-    sum += num;
+  // let nums = the first element (least) to add up from there
+  // nums <= second element in the arr since that will be the highest number
+
+  for (let nums = sortedArr[0]; nums <= sortedArr[1]; nums++) {
+    sum += nums;
+    console.log(`Number being added is: ${nums}`);
+    console.log(sum);
   }
-  console.log('Total is: ' + sum);
-  return sum;
+  console.log(`Total sum: ${sum}`);
+
+  return 1;
 }
 
 sumAll([1, 4]);
+sumAll([15, 2]);
 
-// Time 27:34
-// Grade 1
+// Time: 09:03
+// Grade: 2
+
+// function sumAll(arr) {
+//   // sort the array smallest - largest
+//   arr.sort(function (a, b) {
+//     return a - b;
+//   });
+//   console.log(arr);
+
+//   let sum = 0;
+
+//   for (let num = arr[0]; num <= arr[1]; num++) {
+//     // console.log(num);
+//     sum += num;
+//   }
+//   console.log('Total is: ' + sum);
+//   return sum;
+// }
+
+// sumAll([1, 4]);
