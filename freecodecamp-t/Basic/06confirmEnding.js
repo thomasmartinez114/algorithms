8e6d56e830627069202ccfb4a63540822e5b0b20
tmartinez114 <tmartinez114@gmail.com>
2019-10-21 12:18:36 -0400

    completed 06

--- freecodecamp-t/Basic/06confirmEnding.js
+++ freecodecamp-t/Basic/06confirmEnding.js
@@ -1,13 +1,31 @@
 function confirmEnding(str, target) {
-  // console.log(str.substr(-1)); // logs substr(-1) = 'n'
+  // "Never give up and good luck will find you."
+  // -- Falcor
+
+  // if the ending of str by target's length equals target
   if (str.substr(-target.length) === target) {
     return true;
   } else {
     return false;
   }
+
+  return str;
 }
 
-confirmEnding('Thomas', 'mas');
+confirmEnding('Bastian', 'n');
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substr
+
+// function confirmEnding(str, target) {
+//   // console.log(str.substr(-1)); // logs substr(-1) = 'n'
+//   if (str.substr(-target.length) === target) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// confirmEnding('Thomas', 'mas');
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substr
 
