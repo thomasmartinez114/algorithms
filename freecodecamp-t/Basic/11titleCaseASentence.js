619dbc322010bf3a6685ce6330eaa234a5f36b21
tmartinez114 <tmartinez114@gmail.com>
2019-10-22 18:57:08 -0400

    completed 11

--- freecodecamp-t/Basic/11titleCaseASentence.js
+++ freecodecamp-t/Basic/11titleCaseASentence.js
@@ -1,20 +1,51 @@
-function titleCase(str) {
-  var words = str.split(' ');
-  // console.log(words); // showing words split
-  var newWords = [];
+// Basic Algorithm Scripting: Title Case a SentencePassed
+// Return the provided string with the first letter of each word capitalized.Make sure the rest of the word is in lower case.
 
-  for (var i = 0; i < words.length; i++) {
-    var capWords = words[i].charAt(0).toUpperCase() + words[i].slice(1);
-    // console.log(capWords);
-    newWords.push(capWords);
-    // console.log(newWords);
+// For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
+
+// Remember to use Read - Search - Ask if you get stuck.Write your own code.
+
+function titleCase(str) {
+  let lowerCase = str.toLowerCase();
+
+  let words = lowerCase.split(' ');
+  console.log(words);
+
+  let newWords = [];
+
+  for (let i = 0; i < words.length; i++) {
+    let capitalize = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    console.log(capitalize);
+    // push to array
+    newWords.push(capitalize);
+    // console.log(`Array of words ${newWords}`);
   }
-  var wordsFinal = newWords.join(' ');
-  console.log(wordsFinal);
-  return wordsFinal;
+  let final = newWords.join(' ');
+  console.log(final);
+  return final;
 }
 
 titleCase("I'm a little tea pot");
+titleCase('sHoRt AnD sToUt');
+titleCase('HERE IS MY HANDLE HERE IS MY SPOUT');
+
+// function titleCase(str) {
+//   var words = str.split(' ');
+//   // console.log(words); // showing words split
+//   var newWords = [];
+
+//   for (var i = 0; i < words.length; i++) {
+//     var capWords = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+//     // console.log(capWords);
+//     newWords.push(capWords);
+//     // console.log(newWords);
+//   }
+//   var wordsFinal = newWords.join(' ');
+//   console.log(wordsFinal);
+//   return wordsFinal;
+// }
+
+// titleCase("I'm a little tea pot");
 
 // https://flaviocopes.com/how-to-uppercase-first-letter-javascript/
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
@@ -23,7 +54,6 @@ titleCase("I'm a little tea pot");
 // \\\\\\\\\\\\\\\\\
 // \\ This works  \\
 // \\\\\\\\\\\\\\\\\
-
 
 // function titleCase(str) {
 //   str = str.toLowerCase();
