ee82deb0c43fef1017bc877952f03ed17ea130a1
tmartinez114 <tmartinez114@gmail.com>
2019-11-02 14:24:07 -0400

    worked on fareEstimator

--- codeSignal/fareEstimator.js
+++ codeSignal/fareEstimator.js
@@ -6,20 +6,20 @@ function fareEstimator(
   cost_per_minute,
   cost_per_mile
 ) {
-  // input int for ride_time
-  // input int for ride_distance
+  input int for ride_time
+  input int for ride_distance
 
-  // ((cost per min * ride time) + (cost per mile * ride dist))
-  // cost per min & cost per mile depend on car type
+  ((cost per min * ride time) + (cost per mile * ride dist))
+  cost per min & cost per mile depend on car type
 
-  //     let ridePrice = [];
-  //     let ride_time = 30;
-  //     let ride_distance = 7;
-  //     let cost_per_minute = 10;
-  //     let cost_per_mile = 1;
+      let ridePrice = [];
+      let ride_time = 30;
+      let ride_distance = 7;
+      let cost_per_minute = 10;
+      let cost_per_mile = 1;
 
-  //     let fare = ((cost_per_minute * ride_time) + (cost_per_mile * ride_distance));
-  //     console.log(fare);
+      let fare = ((cost_per_minute * ride_time) + (cost_per_mile * ride_distance));
+      console.log(fare);
 
   for (let carMin = 0; carMin < cost_per_minute.length; carMin++) {
     console.log(cost_per_minute[carMin]);
