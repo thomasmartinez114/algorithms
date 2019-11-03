0de0d84a0012b91897ad2c8f158cb14ec8995f17
tmartinez114 <tmartinez114@gmail.com>
2019-11-03 13:28:16 -0500

    completed 04:01

---
+++ coderByte/firstReverse.js
@@ -0,0 +1,12 @@
+function FirstReverse(str) {
+  var arr = str.split('');
+  arr.reverse();
+  return arr.join('');
+}
+
+// keep this function call here
+// to see how to enter arguments in JavaScript scroll down
+FirstReverse(readline());
+
+// Time: 04:01
+// Grade: 2
