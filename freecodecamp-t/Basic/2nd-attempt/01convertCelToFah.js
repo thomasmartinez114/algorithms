4c6cea8add43985c873eac23c2a5f130d7d667c8
tmartinez114 <tmartinez114@gmail.com>
2019-11-12 18:27:52 -0500

    completed 02:30

---
+++ freecodecamp-t/Basic/2nd-attempt/01convertCelToFah.js
@@ -0,0 +1,17 @@
+// Basic Algorithm Scripting: Convert Celsius to FahrenheitPassed
+// The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9 / 5, plus 32.
+
+// You are given a variable celsius representing a temperature in Celsius.Use the variable fahrenheit already defined and assign it the Fahrenheit temperature equivalent to the given Celsius temperature.Use the algorithm mentioned above to help convert the Celsius temperature to Fahrenheit.
+
+// C to F = C * 9/5 + 32
+
+function convertToF(celsius) {
+    let fahrenheit = celsius * 9 / 5 + 32;
+    console.log(fahrenheit);
+    return fahrenheit;
+}
+
+convertToF(30);
+
+// Time: 02:30
+// Grade: 5
