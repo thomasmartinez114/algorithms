c426af59bdcb2361ff04b6aa661cb0976d311116
tmartinez114 <tmartinez114@gmail.com>
2019-11-23 21:36:25 -0500

    completed 21:16

--- freecodecamp-t/Basic/2nd-attempt/16chunkyMonkey.js
+++ freecodecamp-t/Basic/2nd-attempt/16chunkyMonkey.js
@@ -1,2 +1,18 @@
 // Write a function that splits an array(first argument) into groups the length of size(second argument) and returns them as a two - dimensional array.
 
+function chunkArrayInGroups(arr, size) {
+    let newArray = [];
+    // newArray.push(arr);
+    // console.log(newArray);
+
+    for (var i = 0; i < arr.length; i += size) {
+        newArray.push(arr.slice(i, i + size));
+    }
+    console.log(newArray);
+    return newArray;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+// Time: 21:16
+// Grade: 1
