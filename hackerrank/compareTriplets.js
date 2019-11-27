83c8f6fbb96b7375bd1f3a311ad24e9b7dfcc747
tmartinez114 <tmartinez114@gmail.com>
2019-11-26 19:24:56 -0500

    completed 29:30

--- hackerrank/compareTriplets.js
+++ hackerrank/compareTriplets.js
@@ -8,19 +8,23 @@ function compareTriplets(a, b) {
     let totalScores = [0, 0]
     // [a, b]
 
-    for (let score = 0; score < a.length; score++){
+    for (let score = 0; score < a.length; score++) {
         // console.log(a[score]);
         // console.log(b[score]);
-        if (a[score] > b[score]){
+        if (a[score] > b[score]) {
             totalScores[0]++;
         }
 
-        if (b[score] > a[score]){
+        if (b[score] > a[score]) {
             totalScores[1]++;
         }
     }
     console.log(totalScores);
+    return totalScores;
 }
+
+// Time: 29:30
+// Grade: 5
 
 
 compareTriplets([1, 2, 3], [0, 2, 4]);
