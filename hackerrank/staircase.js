4427d1a5967750d9e52bfcc6f97214c37e36c808
tmartinez114 <tmartinez114@gmail.com>
2019-11-30 17:19:18 -0500

    referencing mario

--- hackerrank/staircase.js
+++ hackerrank/staircase.js
@@ -1,16 +1,21 @@
 function staircase(n) {
-    for (var i = 1; i <= n; i++) {
-        // column
-        for (var k = 0; k < n - i; k++) {
-            console.log(" ");
-        }
-        for (var j = 1; j <= i; j++) {
-            // row
-            console.log("#");
-        }
-        console.log("\n");
-    }
 
 }
 
 staircase(4);
+
+// Loop to do hashtags and pyramid
+// for (int i = 1; i <= height; i++)
+// {
+//     // column
+//     for (int k = 0; k < height - i; k++)
+//     {
+//         printf(" ");
+//     }
+//     for (int j = 1; j <= i; j++)
+//     {
+//         // row
+//         printf("#");
+//     }
+//     printf("\n");
+// }
