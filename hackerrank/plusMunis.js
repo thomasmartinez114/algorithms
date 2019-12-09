f89e3aa208384b8daf26a659c30d37c193ec3c30
tmartinez114 <tmartinez114@gmail.com>
2019-12-09 18:33:03 -0500

    notes

---
+++ hackerrank/plusMunis.js
@@ -0,0 +1,11 @@
+// Given an array of integers, calculate the fractions of its elements that are positive, negative, and are zeros.Print the decimal value of each fraction on a new line.
+
+// Note: This challenge introduces precision problems.The test cases are scaled to six decimal places, though answers with absolute error of up to  are acceptable.
+
+// For example, given the array  there are  elements, two positive, two negative and one zero.Their ratios would be, 2/5 positives, 2/5 negatives, 1/5 zeros and. It should be printed as:
+// 0.400000
+// 0.400000
+// 0.200000
+
+// print each on their own line rounded to 6th place
+// arr is the array of intergers
