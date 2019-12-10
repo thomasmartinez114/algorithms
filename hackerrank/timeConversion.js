c4c121c72d76cd44864e4f42f0101e19daf39c6b
tmartinez114 <tmartinez114@gmail.com>
2019-12-10 18:33:30 -0500

    pushed intro

---
+++ hackerrank/timeConversion.js
@@ -0,0 +1,16 @@
+// Given a time in 12 hour AM/PM format, convert it to military 24 format.
+// Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+//timeConversion has the following parameter(s):
+// s: a string representing time in 12 hour format
+
+// Sample Input
+// 07:05:45PM
+
+// Sample Output
+// 19:05:45
+
+function timeConversion(s) {
+  console.log(s);
+}
+
+timeConversion('07:05:45PM');
