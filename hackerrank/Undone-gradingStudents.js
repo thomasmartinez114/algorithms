0c74afe521104ae51ace86e5416cbc80d39471b3
tmartinez114 <tmartinez114@gmail.com>
2019-12-13 13:55:47 -0500

    need to finish

---
+++ hackerrank/Undone-gradingStudents.js
@@ -0,0 +1,21 @@
+// Every student gets grade 0 - 100
+// Grade less than 40 is failing
+
+// If difference of grade and the next multple of 5 is < 3 round up to nearest 5
+// If less than 38, doesn't matter they're failing anyway
+
+// Ex) 84 -> 85
+// Ex) 29 nothing since failing
+
+// Sample Input
+// 4
+// 73
+// 67
+// 38
+// 33
+
+// Sample Output
+// 75
+// 67
+// 40
+// 33
---
+++ hackerrank/Undone-timeConversion.js
@@ -0,0 +1,44 @@
+// Given a time in 12 hour AM/PM format, convert it to military 24 format.
+// Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+//timeConversion has the following parameter(s):
+// s: a string representing time in 12 hour format
+
+// Sample Input
+// 07:05:45PM
+
+// Sample Output
+// 19:05:45
+
+function timeConversion(s) {
+  // split from :
+  // then take the first element and subrtract by 24 to turn into Military time
+  // split last by PM or AM
+
+  let newTime = s.slice(0, 8).split(':'); // sliced from [0] - [8] to not grab AMPM
+  console.log(newTime);
+
+  let hour = 0;
+  let min = 0;
+  let sec = 0;
+
+  for (var i = 0; i < newTime.length; i++) {
+    if (newTime > 12) {
+      hour = 12 + newTime[0];
+    } else {
+      hour = 12 + parseFloat(newTime[0]); // convert 07 to int
+      //   console.log(hour);
+    }
+    min = newTime[1];
+    sec = newTime[2];
+  }
+
+  console.log(hour + ':' + min + ':' + sec);
+  return hour + ':' + min + ':' + sec;
+}
+
+timeConversion('07:05:45PM');
+
+// failed the other tests need to resolve
+
+// Time: 54:45
+// Grade: 5
