f477d0f4b1678fa74f93a8ceef0168e0f6d9a67e
tmartinez114 <tmartinez114@gmail.com>
2019-12-12 20:27:17 -0500

    pushed alg direction

---
+++ hackerrank/gradingStudents.js
@@ -0,0 +1,21 @@
+// Every student gets grade 0 - 100
+// Grade less than 40 is failing
+
+// Round up to nearest multiple of 5
+// If less than 38, doesn't matter they're failing anyway
+
+// Ex) 84 -> 85
+// Ex) 29 nothing since failing
+
+// Sample Input
+// 4
+// 73
+// 67
+// 38
+// 33
+
+// Sample Output
+// 75
+// 67
+// 40
+// 33
