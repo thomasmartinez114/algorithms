f2998d2626d317c246d0a9f8ef1e4ca1eae3757b
tmartinez114 <tmartinez114@gmail.com>
2019-12-14 12:16:36 -0500

    added notes

---
+++ hackerrank/appleAndOrange.js
@@ -0,0 +1,26 @@
+// Sam's house is between s = 7 and t = 10
+
+// Apple tree is a = 4
+// Orange tree is b = 12
+
+// m = 3 apples
+// n = 3 oranges
+
+// apples are thrown [2, 3, -4] from a
+// oranges are thrown [3, -2, 4] from b
+
+// sum of apples from tree [6, 7, 0]
+// sum of oranges from tree [15, 10, 8]
+
+// 1 apple and 2 oranges land in Sam's house range
+
+// s: integer, starting point of Sam's house location.
+// t: integer, ending location of Sam's house location.
+// a: integer, location of the Apple tree.
+// b: integer, location of the Orange tree.
+// apples: integer array, distances at which each apple falls from the tree.
+// oranges: integer array, distances at which each orange falls from the tree.
+
+// print out two lines
+// 1) apples that fall on sam's house
+// 2) oranges that fall on sam's house
