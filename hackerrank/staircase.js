9438ff4f417d3e2914e684a78eba715461e9a875
tmartinez114 <tmartinez114@gmail.com>
2019-12-20 19:56:47 -0500

    pushed staircase

--- hackerrank/staircase.js
+++ hackerrank/staircase.js
@@ -1,29 +1,15 @@
 function staircase(n) {
-    for (var i = 1; i <= n; i++){
-        for (var j = 0; j < n - 1; j++){
-            console.log(".");
-        }
-        for (var k = 1; k <= i; k++){
-            console.log("#");
-        }
-        console.log("\n");
+  for (var i = 0; i <= n; i++) {
+    // column
+    for (var j = 0; j < n - 1; j++) {
+      console.log(' ');
     }
+    for (var k = 1; k <= i; j++) {
+      // row
+      console.log('#');
+    }
+    console.log('\n');
+  }
 }
 
 staircase(4);
-
-// Loop to do hashtags and pyramid
-// for (int i = 1; i <= height; i++)
-// {
-//     // column
-//     for (int k = 0; k < height - i; k++)
-//     {
-//         printf(" ");
-//     }
-//     for (int j = 1; j <= i; j++)
-//     {
-//         // row
-//         printf("#");
-//     }
-//     printf("\n");
-// }
