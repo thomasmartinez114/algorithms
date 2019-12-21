8af1d3ae0857233c1c4d7d90d4dd0a34530a60f2
tmartinez114 <tmartinez114@gmail.com>
2019-12-20 20:04:20 -0500

    trying to resovle nonstop # prints

--- hackerrank/staircase.js
+++ hackerrank/staircase.js
@@ -1,14 +1,15 @@
 function staircase(n) {
-  for (var i = 0; i <= n; i++) {
+  for (var i = 0; i < n; i++) {
     // column
-    for (var j = 0; j < n - 1; j++) {
-      console.log(' ');
+    for (var j = 0; j <= n; j++) {
+      if (i + j < n - 1) {
+        console.log(' ');
+      }
     }
     for (var k = 1; k <= i; j++) {
       // row
       console.log('#');
     }
-    console.log('\n');
   }
 }
 
