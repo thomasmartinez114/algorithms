1ceac57b062462c001dc8e0e63fe3890219c71c3
tmartinez114 <tmartinez114@gmail.com>
2019-12-20 20:06:51 -0500

    now printing nothing... will troubleshoot

--- hackerrank/staircase.js
+++ hackerrank/staircase.js
@@ -1,14 +1,10 @@
 function staircase(n) {
   for (var i = 0; i < n; i++) {
-    // column
     for (var j = 0; j <= n; j++) {
       if (i + j < n - 1) {
         console.log(' ');
       }
-    }
-    for (var k = 1; k <= i; j++) {
-      // row
-      console.log('#');
+      console.log('\n');
     }
   }
 }
