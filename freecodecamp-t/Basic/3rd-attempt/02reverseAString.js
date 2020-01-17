3289e61bc55547b3ba1c08850f4fa44cebf75e9a
tmartinez114 <tmartinez114@gmail.com>
2020-01-17 16:59:32 -0500

    completed 1:56

---
+++ freecodecamp-t/Basic/3rd-attempt/02reverseAString.js
@@ -0,0 +1,20 @@
+// Basic Algorithm Scripting: Reverse a StringPassed
+// Reverse the provided string.
+
+// You may need to turn the string into an array before you can reverse it.
+
+// Your result must be a string.
+
+// Remember to use Read - Search - Ask if you get stuck.Write your own code.
+
+function reverseString(str) {
+  return str
+    .split('')
+    .reverse()
+    .join('');
+}
+
+reverseString('hello');
+
+// Time: 1:56
+// Grade: 5
