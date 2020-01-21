ceb1586798729c0d05b774f9fed7c82510ded6d2
tmartinez114 <tmartinez114@gmail.com>
2020-01-21 18:41:45 -0500

    completed 06

---
+++ freecodecamp-t/Basic/3rd-attempt/06confirmEnding.js
@@ -0,0 +1,22 @@
+// Basic Algorithm Scripting: Confirm the EndingPassed
+// Check if a string(first argument, str) ends with the given target string(second argument, target).
+
+// This challenge can be solved with the.endsWith() method, which was introduced in ES2015.But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+
+// Remember to use Read - Search - Ask if you get stuck.Write your own code.
+
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+
+  // if the ending of str by target's length equals target
+  if (str.substr(-target.length) === target) {
+    return true;
+  } else {
+    return false;
+  }
+
+  return str;
+}
+
+confirmEnding('Bastian', 'n');
